4d820c5a58a99d1c2d430218ae0c1e0fa2b6db76
guangxq <guangxq@163.com>
2016-07-25 10:17:19 +0800

    Initial Commit

---
+++ SoundGuide/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  SoundGuide
+//
+//  Created by Mac on 16/7/25.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SoundGuide/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  SoundGuide
+//
+//  Created by Mac on 16/7/25.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
